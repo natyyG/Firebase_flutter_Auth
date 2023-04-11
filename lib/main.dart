4168d35b147e51cf842cd75233eef1b306340feb
natyyG <natnalecs16@gmail.com>
2023-04-11 03:14:19 -0700

    created signin layout with email and password field

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -47,13 +38,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  late String _email;
+  late String _password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              textInputAction: TextInputAction.next,
+              decoration: InputDecoration(hintText: "Enter your email"),
+            ),
+            TextField(
+              textInputAction: TextInputAction.next,
+              decoration: InputDecoration(hintText: "Enter your password"),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(onPressed: () {}, child: Text("Sign in"))
+          ],
+        ),
       ),
-      body: Center(child: Container(child: Text("this is text"))),
     );
   }
 }
